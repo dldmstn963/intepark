b9baeca816990d3c2415ca5c3706b50b31c291c8
laopos <laopos@naver.com>
2019-12-06 19:25:34 +0900

    영수~~~

--- src/main/java/com/c4/intepark/HomeController.java
+++ src/main/java/com/c4/intepark/HomeController.java
@@ -40,7 +40,7 @@ public class HomeController {
 	public String main(Model model) {
 		logger.info("Welcome main!");
 
-		return "TemplateFolder/main";
+		return "main";
 	}
 	
 	@RequestMapping("auctionList.do")
