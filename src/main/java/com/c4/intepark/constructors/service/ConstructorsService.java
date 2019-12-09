8c0d1111eef2c9ff1f2b67f8507ebc223ee1654d
HoSungYang <yhsto001@gmail.com>
2019-12-09 19:34:32 +0900

    패키지 지워주세요

--- src/main/java/com/c4/intepark/constructors/service/ConstructorsService.java
+++
@@ -1,7 +0,0 @@
-package com.c4.intepark.constructors.service;
-
-import com.c4.intepark.constructors.vo.Constructors;
-
-public interface ConstructorsService {
-	Constructors loginCheck(Constructors cons);
-}
